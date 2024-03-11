36c06704922bcf4d378a53ab9607f0d2f73e9323
irensmile <curkan.ira@gmail.com>
2024-03-11 00:54:22 -0400

    initial and (I hope) final commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class Home {
+  private tenants: Person[] = [];
+  protected door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome Home!");
+    } else throw "The Door is Closed!";
+  }
+
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends Home {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+    return this.door;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
